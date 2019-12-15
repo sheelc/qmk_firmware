c24743f1ec222aa1932ce0ce319b5f656d97d104
Sheel Choksi <sheelchoksi@gmail.com>
2019-12-14 23:10:11 -0800

    Fix incorrect mod

--- keyboards/massdrop/ctrl/keymaps/sheelc/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/sheelc/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,          KC_W,          KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,          KC_P,     KC_LBRC,       KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
         KC_CAPS,       KC_A,          KC_S,          KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,          KC_SCLN,  KC_QUOT,       KC_ENT, \
         OSM(MOD_LSFT), KC_Z,          KC_X,          KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,        KC_SLSH,  OSM(MOD_RSFT),                              KC_UP, \
-        OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LCMD),                   KC_SPC,                             OSM(MOD_RCMD), MO(_FNC), KC_APP,        OSM(MOD_RCTL),      KC_LEFT, KC_DOWN, KC_RGHT \
+        OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI),                   KC_SPC,                             OSM(MOD_RGUI), MO(_FNC), KC_APP,        OSM(MOD_RCTL),      KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [_FNC] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            KC_MUTE, _______, _______, \
